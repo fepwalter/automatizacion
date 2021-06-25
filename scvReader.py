f5436f68e408576eaa74ef2e064cbd6a32e62f37
Facu07 <facuwalter7@gmail.com>
2021-06-25 19:20:50 -0300

    Obtiene cada unico y novedad del csv

--- scvReader.py
+++ scvReader.py
@@ -1,6 +1,16 @@
 import csv as csv
 
+
+# Leer el archivo 'datos.csv' con reader() y 
+# realizar algunas operaciones básicas: 
+
 with open('prueba.csv', newline='') as File:  
-    reader = csv.DictReader(File)
+    reader = csv.reader(File, delimiter=';')
     for row in reader:
-        print(row['unico'])
+    	num_unico = row[0]
+    	novedad = row[1]
+    	print(num_unico,novedad)
+del num_unico, novedad, reader  # Borrar objetos
+File.close()  # Cerrar archivo
+del File  
+
