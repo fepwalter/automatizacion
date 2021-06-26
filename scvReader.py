3902e531fa73d39dfacd3e28b2cc608bf563976b
fepwalter <fepwalter@gmail.com>
2021-06-25 22:59:41 -0300

    Leer archivos Excel
    codificando a utf-8

--- scvReader.py
+++ scvReader.py
@@ -3,21 +3,14 @@ import csv as csv
 
 # Leer el archivo 'datos.csv' con reader() y 
 # realizar algunas operaciones básicas: 
+with open("pruba1.csv","w",newline ='') as csvFile:
+	writer = csv.writer(csvFile, delimiter=',', quoting = 'quote_minimal')
+	with open('Hoja de Ruta Deborah Salida3 - Hoja 1.csv', newline='', encoding="utf-8") as File:  
+		reader = csv.reader(File, delimiter=',')
+		for row in reader:
+			#entrada = row[1]
+			writer.writerow(row[1])
+			
 
-with open('prueba.csv', newline='') as File:  
-    reader = csv.reader(File, delimiter=';')
-    for row in reader:
-    	num_unico = row[0]
-    	novedad = row[1]
-    	if novedad == '22':
-    		print("entregado")
-    	elif novedad.lower() == 'rae':
-    		print("Se retiro")
-    	elif novedad.lower() == 'not':
-    		print("notificacion")
-    	elif novedad.lower() == 'ua':
-    		print("unico aviso")
-    		
-del num_unico, novedad, reader  # Borrar objetos
 File.close()  # Cerrar archivo
 del File  
