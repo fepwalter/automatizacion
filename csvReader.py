8f24fcc93ac6afe58d824dfdc19fd660fa46891e
fepwalter <fepwalter@gmail.com>
2021-06-26 17:08:55 -0300

    Correccion del delimitador
    Se cambio el delimitador reader

--- csvReader.py
+++ csvReader.py
@@ -3,10 +3,10 @@ import csv as csv
 
 # Leer el archivo 'datos.csv' con reader() y 
 # realizar algunas operaciones básicas: 
-with open("pruba1.csv","w",newline ='') as csvFile:
+with open("prueba1.csv","w",newline ='') as csvFile:
 	writer = csv.writer(csvFile, delimiter=';')
-	with open('Hoja de Ruta Deborah Salida3 - Hoja 1.csv', newline='', encoding="utf-8") as File:
-		reader = csv.reader(File, delimiter=',')
+	with open('Hoja de Ruta Deborah Salida3.csv', newline='', encoding="ANSI") as File:
+		reader = csv.reader(File, delimiter=';')
 		for row in reader:
 			writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
 
