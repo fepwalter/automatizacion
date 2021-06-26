9cff97a881fddc7cb3e1287a988f40ec0eabbb79
fepwalter <fepwalter@gmail.com>
2021-06-25 23:35:59 -0300

    Imprimir columnas 1,37 y 38

--- scvReader.py
+++ scvReader.py
@@ -4,13 +4,15 @@ import csv as csv
 # Leer el archivo 'datos.csv' con reader() y 
 # realizar algunas operaciones básicas: 
 with open("pruba1.csv","w",newline ='') as csvFile:
-	writer = csv.writer(csvFile, delimiter=',', quoting = 'quote_minimal')
+	writer = csv.writer(csvFile, delimiter=',')
 	with open('Hoja de Ruta Deborah Salida3 - Hoja 1.csv', newline='', encoding="utf-8") as File:  
 		reader = csv.reader(File, delimiter=',')
-		for row in reader:
-			#entrada = row[1]
-			writer.writerow(row[1])
-			
+		try:
+			for row in reader:
+				#entrada = row[1]
+				writer.writerow([row[1],row[37],row[38]])
+		except
+
 
 File.close()  # Cerrar archivo
 del File  
