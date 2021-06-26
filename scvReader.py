76a11a40344198bc9b0cf6c95242acefcf8e66e9
Facu07 <facuwalter7@gmail.com>
2021-06-26 16:50:10 -0300

    Se lee sin problemas y escribe
    Lee sin problemas el archivo.
    Genera un csv utilizable

--- scvReader.py
+++ scvReader.py
@@ -4,16 +4,11 @@ import csv as csv
 # Leer el archivo 'datos.csv' con reader() y 
 # realizar algunas operaciones básicas: 
 with open("pruba1.csv","w",newline ='') as csvFile:
-	writer = csv.writer(csvFile, delimiter=',')
-	with open('Hoja de Ruta Deborah Salida3 - Hoja 1.csv', newline='', encoding="utf-8") as File:  
+	writer = csv.writer(csvFile, delimiter=';')
+	with open('Hoja de Ruta Deborah Salida3 - Hoja 1.csv', newline='', encoding="utf-8") as File:
 		reader = csv.reader(File, delimiter=',')
 		for row in reader:
-			try:
-				#entrada = row[1]
-				writer.writerow([row[1],row[37],row[38]])
-			except:
-				print("vacio")
-
+			writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
 
 File.close()  # Cerrar archivo
 del File  
