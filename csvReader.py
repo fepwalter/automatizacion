c89b8acfe2ef88ae66f83628b7c7a76e12ee967c
Facu07 <facuwalter7@gmail.com>
2021-06-26 17:54:36 -0300

    Procesa multiples .csv
    -Encuentra todos los archivos .csv de la carpeta de entrada
    -Genera un unico archivo salida.csv en la carpeta salida, con los datos extraídos de los .csv de la carpeta de entrada

--- csvReader.py
+++ csvReader.py
@@ -1,14 +1,18 @@
 import csv as csv
+import os
 
+dir_entrada = os.getcwd()+"\\entrada\\"
+dir_salida = os.getcwd()+"\\salida\\salida.csv"
 
-# Leer el archivo 'datos.csv' con reader() y 
-# realizar algunas operaciones básicas: 
-with open("prueba1.csv","w",newline ='') as csvFile:
-	writer = csv.writer(csvFile, delimiter=';')
-	with open('Hoja de Ruta Deborah Salida3.csv', newline='', encoding="ANSI") as File:
-		reader = csv.reader(File, delimiter=';')
-		for row in reader:
-			writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
+for csv_file in os.listdir(dir_entrada):
+	# Leer el archivo 'datos.csv' con reader() y 
+	# realizar algunas operaciones básicas: 
+	with open(dir_salida,"w",newline ='') as csvFile:
+		writer = csv.writer(csvFile, delimiter=';')
+		with open(dir_entrada+csv_file, newline='', encoding="ANSI") as File:
+			reader = csv.reader(File, delimiter=',')
+			for row in reader:
+				writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
 
 File.close()  # Cerrar archivo
 del File  
