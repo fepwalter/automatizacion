28915d67670c25a734cb5f22b4aa9f3387872217
fepwalter <fepwalter@gmail.com>
2021-06-26 18:59:00 -0300

    Agregue codigo al robot, cambio del delimitador
    Cambié el delimitador del csv.Reader por ;
    Agregué el codigo para cargar novedades del 2021

--- csvReader.py
+++ csvReader.py
@@ -2,15 +2,15 @@ import csv as csv
 import os
 
 dir_entrada = os.getcwd()+"\\entrada\\"
-dir_salida = os.getcwd()+"\\salida\\salida.csv"
+dir_salida = os.getcwd()+"\\salida\\"
 
-for csv_file in os.listdir(dir_entrada):
+for i, csv_file in enumerate (os.listdir(dir_entrada)):
 	# Leer el archivo 'datos.csv' con reader() y 
 	# realizar algunas operaciones básicas: 
-	with open(dir_salida,"w",newline ='') as csvFile:
+	with open(dir_salida+"salida"+str(i)+".csv","w",newline ='') as csvFile:
 		writer = csv.writer(csvFile, delimiter=';')
 		with open(dir_entrada+csv_file, newline='', encoding="ANSI") as File:
-			reader = csv.reader(File, delimiter=',')
+			reader = csv.reader(File, delimiter=';')
 			for row in reader:
 				writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
 
--- robot.py
+++ robot.py
@@ -1,20 +1,23 @@
 import pyautogui as robot
 import time
 import csv
+from tkinter import messagebox
+
 
 pos_aplicacion = 57,500
+pos_anoOperativo = 1046,355
 pos_numero_unico = 955,392
 pos_novedad = 937,490
 pos_cargar = 1129,392
 pos_asignar = 907,695
-
+archivo_csv = "robot.csv"
 
 def moverMouse(pos,click=1):
 	robot.moveTo(pos)
 	robot.click(clicks=click)
 
 def entrarAplicativoAsignarNovedad():
-	moverMouse(pos_seguimiento, 2)
+	moverMouse(pos_aplicacion, 2)
 	robot.sleep(1)
 	robot.hotkey("down")
 	robot.hotkey("tab")
@@ -42,12 +45,17 @@ def asignar():
 	moverMouse(pos_asignar)
 	robot.hotkey("enter")
 
+def anoOperativo2021():
+	robot.hotkey("up")
+	robot.hotkey("enter")
+	robot.sleep(1)
+
 # leer csv, obtener campos del archivo
 # Leer el archivo 'datos.csv' con reader() y 
 # realizar algunas operaciones básicas: 
 
-entrarAplicativoAsignarNovedad()
-with open('prueba.csv', newline='') as File:  
+entrarAplicativoAsignarNovedad() # entramos al aplicativo a asignar novedad
+with open(archivo_csv, newline='') as File:  
     reader = csv.reader(File, delimiter=';')
     for row in reader:
     	num_unico = row[0]
@@ -67,7 +75,33 @@ with open('prueba.csv', newline='') as File:
     	intro()
     	cargar()
     	asignar()
-    		
+File.close()
+with open(archivo_csv, newline='') as File:  
+    reader = csv.reader(File, delimiter=';')
+    #Paso a leer la columna 3 cono las novedades del 2021.
+    moverMouse(pos_anoOperativo)
+    anoOperativo2021()
+    for row in reader:
+    	num_unico = row[0]
+    	novedad = row[2]
+    	moverMouse(pos_numero_unico) # meter numero unico
+    	robot.typewrite(num_unico)
+    	robot.sleep(1)
+    	moverMouse(pos_novedad) # Posisionar en novedad
+    	if novedad == '22':
+    		tipearNovedad("2", 3)
+    	elif novedad.lower() == 'rae':
+    		tipearNovedad("r", 1)
+    	elif novedad.lower() == 'not':
+    		tipearNovedad("n", 2)
+    	elif novedad.lower() == 'ua':
+    		tipearNovedad("u", 1)
+    	intro()
+    	cargar()
+    	asignar()
+
 del num_unico, novedad, reader  # Borrar objetos
 File.close()  # Cerrar archivo
-del File  
+del File
+
+messagebox.showinfo(message="Terminé craa", title="Happy Hour")
