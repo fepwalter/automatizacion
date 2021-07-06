db5b85d778717a1fc788259374e66e9b75345e7b
fepwalter <fepwalter@gmail.com>
2021-07-06 16:21:19 -0300

    Versionado nuevo del programa
    Se crea una clase con los codigos a utilizar(faltan agregar codigos)
    Ahora el csvReader contempla novedades que no tenga que cargar (completando la salida con "null")
    El robot saltea las novedades null y carga el resto

--- csvReader.py
+++ csvReader.py
@@ -1,10 +1,22 @@
 import csv as csv
 import os
+import codigos_internos as ci
 
 #obtener la direccion de los archivos
 dir_entrada = os.getcwd()+"\\entrada\\"
 dir_salida = os.getcwd()+"\\salida\\"
 
+def cargarNovedad(cu,nov2020,nov2021,aCargar,obs):
+	if aCargar.lower() == ci.COD_NINGUNO:
+		return
+	elif aCargar.lower() == ci.COD_AMBOS:
+		writer.writerow([cu]+[nov2020]+[nov2021]+[obs])
+	elif aCargar.lower() == ci.COD_2020:
+		writer.writerow([cu]+[nov2020]+["null"]+[obs])
+	elif aCargar.lower() == ci.COD_2021:	
+		writer.writerow([cu]+["null"]+[nov2021]+[obs])
+	#print(aCargar)
+	
 #for i, csv_file in enumerate (os.listdir(dir_entrada)): este sirve si queres crear archivos de salida distintos
 #with open(dir_salida+"nombre"+str(i)+".csv","w",newline='') as csvFile:
 for csv_file in os.listdir(dir_entrada):
@@ -13,9 +25,12 @@ for csv_file in os.listdir(dir_entrada):
 		with open(dir_entrada+csv_file, newline='', encoding="ANSI") as File:
 			reader = csv.reader(File, delimiter=';')
 			for row in reader:
+				if not row[40]:
+					row[40]= ci.COD_NO_OBS
+				cargarNovedad(row[1],row[37],row[38],row[39],row[40])
 				#solo controla por fila (si solo hay que cambiar novedad de algun año no lo comprueba eso)
-				if row[39].lower() == "true": 
-					writer.writerow([row[1]]+[row[37]]+[row[38]]) #+ para agregar celdas al lado
+				#if row[39].lower() == "1": 
+					#writer.writerow([row[1]]+[row[37]]+[row[38]]+[row[40]]) #+ para agregar celdas al lado
 
 File.close()  # Cerrar archivo
 del File  
--- robot.py
+++ robot.py
@@ -3,36 +3,66 @@ import time
 import csv
 from tkinter import messagebox
 import os
+import codigos_internos as ci
 
 dir_salida = os.getcwd()+"\\salida\\"
 pos_aplicacion = 57,500
+archivo_csv = dir_salida+"archivodecarga.csv"
+
+#variables de posicion para el modulo de asignado de novedad
 pos_anoOperativo = 1046,355
 pos_numero_unico = 955,392
 pos_novedad = 937,490
 pos_cargar = 1129,392
 pos_asignar = 907,695
-archivo_csv = "robot.csv"
+pos_cuadro_observ = 950,550
+pos_cerrar_asig = 1163,303
+
+#variables de posicion para el modulo de Notificaciones
+pos_cargar_unico = 870,453
+pos_contacto = 830,517
+pos_numero_not = 830,551
+pos_cerrar_nots = 1280,371
+pos_operativo_not = 1006,414
 
 def moverMouse(pos,click=1):
 	robot.moveTo(pos)
 	robot.click(clicks=click)
 
-def entrarAplicativoAsignarNovedad():
+def irAmenu(hotkey,veces):
+	robot.hotkey("alt")
+	robot.typewrite("a")
+	for i in range(veces):
+		robot.hotkey(hotkey)
+
+def irAmenuNots():
+	irAmenu("down",4)
+	robot.hotkey("right")
+	robot.hotkey("enter")
+	robot.hotkey("enter")
+	robot.sleep(1)
+
+def irAmenuAsignar():	
+	irAmenu("right",1)
+	robot.hotkey("enter")
+	robot.sleep(1)
+
+def entrarAplicativo():
 	moverMouse(pos_aplicacion, 2)
 	robot.sleep(1)
 	robot.hotkey("down")
 	robot.hotkey("tab")
 	robot.hotkey("enter")
 	robot.sleep(2)
-	robot.hotkey("alt")
-	robot.typewrite("a")
-	robot.hotkey("right")
-	robot.hotkey("enter")
-	robot.sleep(1)
 
-def tipearNovedad(tecla, veces):
+def tipearNovedad(tecla, veces, num_unico):
+	moverMouse(pos_numero_unico) # meter numero unico
+	robot.typewrite(num_unico)
+	robot.sleep(1)
+	robot.hotkey("tab")#moverMouse(pos_novedad) # Posisionar en novedad
 	for x in range(veces):
 		robot.typewrite(tecla)
+	robot.hotkey("tab")
 	robot.sleep(1)
 
 def intro():
@@ -41,79 +71,98 @@ def intro():
 
 def cargar():
 	moverMouse(pos_cargar)
+	robot.sleep(1)
 
 def asignar():
 	moverMouse(pos_asignar)
 	robot.hotkey("enter")
 
-def anoOperativo2021():
+def posicionarOperativo2021():
+	moverMouse(pos_anoOperativo)
 	robot.hotkey("up")
 	robot.hotkey("enter")
 	robot.sleep(1)
 
-# leer csv, obtener campos del archivo
-# Leer el archivo 'datos.csv' con reader() y 
-# realizar algunas operaciones básicas: 
-
-entrarAplicativoAsignarNovedad() # entramos al aplicativo a asignar novedad
-
-with open(dir_salida+"archivodecarga.csv", newline='') as File:  
-    reader = csv.reader(File, delimiter=';')
-    for row in reader:
-    	num_unico = row[0]
-    	novedad = row[1]
-    	moverMouse(pos_numero_unico) # meter numero unico
-    	robot.typewrite(num_unico)
-    	robot.sleep(1)
-    	moverMouse(pos_novedad) # Posisionar en novedad
-    	if novedad == '22':
-    		tipearNovedad("2", 3)
-    	elif novedad.lower() == 'rae':
-    		tipearNovedad("r", 1)
-    	elif novedad.lower() == 'not':
-    		tipearNovedad("n", 2)
-    	elif novedad.lower() == 'ua':
-    		tipearNovedad("u", 1)
-    	elif novedad.lower() == 'c':
-    		tipearNovedad('c', 1)
-    	elif novedad.lower() == 'cfa':
-    		tipearNovedad('c', 7)
-    	intro()
-    	cargar()
-    	asignar()
-File.close()
-
-
-with open(dir_salida+"archivodecarga.csv", newline='') as File:  
-	reader = csv.reader(File, delimiter=';')
-    #Paso a leer la columna 3 cono las novedades del 2021.
-	moverMouse(pos_anoOperativo)
-	anoOperativo2021()
-	for row in reader:
-		num_unico = row[0]
-		novedad = row[2]
-		moverMouse(pos_numero_unico) # meter numero unico
-		robot.typewrite(num_unico)
-		robot.sleep(1)
-		moverMouse(pos_novedad) # Posisionar en novedad
-		if novedad == '22':
-			tipearNovedad("2", 3)
-		elif novedad.lower() == 'rae':
-			tipearNovedad("r", 1)
-		elif novedad.lower() == 'not':
-			tipearNovedad("n", 2)
-		elif novedad.lower() == 'ua':
-			tipearNovedad("u", 1)
-		elif novedad.lower() == 'c':
-			tipearNovedad('c', 1)
-		elif novedad.lower() == 'cfa':
-			tipearNovedad('c', 7)
+def lectura_carga_novedades(anio):
+	contador = 0
+	with open(archivo_csv, newline='') as File:
+		reader = csv.reader(File, delimiter=';')
+		for row in reader:
+			num_unico = row[0]
+			obsvervacion = row[3]
+			if anio == 2020:
+				novedad = row[1]
+			else:
+				novedad = row[2]
+			if novedad.lower() != 'null':
+				if novedad == '22':
+					tipearNovedad("2", 3, num_unico)
+				elif novedad.lower() == 'rae':
+					tipearNovedad("r", 1, num_unico)
+				elif novedad.lower() == 'not':
+					contador +=1
+					tipearNovedad("n", 2, num_unico)
+				elif novedad.lower() == 'ua':
+					tipearNovedad("u", 1, num_unico)
+				elif novedad.lower() == 'c':
+					tipearNovedad('c', 1, num_unico)
+				elif novedad.lower() == 'cfa':
+					tipearNovedad('c', 7, num_unico)
+				elif novedad.lower() == '26v':
+					tipearNovedad("2",7, num_unico)
+				#comprobar si hay codigo 4(NO_OBS)
+				if obsvervacion != ci.COD_NO_OBS:
+					robot.typewrite(obsvervacion)
+				#intro()
+				cargar()
+				asignar()
+			else:
+				print("no hago nada")
+				
+	File.close()
+	return contador
+	
+def asignarNots(contador):
+	for i in range(contador):
+		moverMouse(pos_cargar_unico)
+		#intro()solo para los casos en que ya haya una not previa
+		moverMouse(pos_contacto)
+		robot.typewrite("not")
+		moverMouse(pos_numero_not)
+		robot.typewrite(str(i))
 		intro()
-		cargar()
-		asignar()
-File.close()
+		intro()
+	moverMouse(pos_cerrar_nots)
+	robot.sleep(1)
+	
+# entramos al aplicativo a asignar novedad
+def main(): 
+	entrarAplicativo()
+	irAmenuAsignar()
+	contadorNot2020 = lectura_carga_novedades(2020)
+	posicionarOperativo2021()
+	contadorNot2021 = lectura_carga_novedades(2021)
+	moverMouse(pos_cerrar_asig)
 
-del num_unico, novedad, reader  # Borrar objetos
-File.close()  # Cerrar archivo
-del File
-messagebox.showinfo(message="Terminé craa", title="Tomando un feca")
+	irAmenuNots()#entra al modulo de not ya en 2020
+	asignarNots(contadorNot2020)
+
+	#para entrar al modulo de not pero al 2021:	
+	irAmenuNots()
+	moverMouse(pos_operativo_not)
+	robot.hotkey("up")
+	robot.hotkey("enter")
+	asignarNots(contadorNot2021)
+
+	#generar los UA en PDF
+	irAmenu('down',3)
+	robot.hotkey('right')
+	robot.hotkey('down')
+	robot.hotkey('down')
+	robot.hotkey('down')
+	robot.hotkey('down')
+	robot.hotkey('enter')
+
+if __name__=='__main__':
+	main()
+	messagebox.showinfo(message="Terminé craa", title="Tomando un feca")
